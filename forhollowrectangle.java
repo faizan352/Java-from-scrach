d35666f5413123ca3c5dc4ff9c8c847a7801626a
Mohd Faizan Ansari <61273197+faizan352@users.noreply.github.com>
2022-11-12 15:55:56 +0530

    Update forhollowrectangle.java

--- forhollowrectangle.java
+++ forhollowrectangle.java
@@ -12,6 +12,9 @@ public class forhollowrectanle {
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		
+		//int n=4;
+		//int m=5;
+		
 		for(int i=1; i<=n; i++) {
 			
 			for(int j =1; j<=m; j++) {
