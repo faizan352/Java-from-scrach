d341f3ba5d113ddbbf3c94ab019f1e8d6fbbf531
Mohd Faizan Ansari <61273197+faizan352@users.noreply.github.com>
2022-11-19 12:55:36 +0530

    Add files via upload

---
+++ Stringz.java
@@ -0,0 +1,20 @@
+package firstJavaProject;
+
+import java.util.Scanner;
+
+public class Stringz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Scanner sc =new Scanner(System.in);
+//		String name =sc.nextLine();		
+		
+		String firstName="faizan";
+		String secondName="ansari";
+		String fullName= firstName + secondName;   //Concatenation
+		System.out.println(fullName);
+
+	}
+
+}
